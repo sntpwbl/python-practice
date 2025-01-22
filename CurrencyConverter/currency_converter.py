2def27ae01d2c57e351f852f995853b9e82dcb87
sntpwbl <joaop401@outlook.com>
2025-01-22 00:27:49 -0300

    refactor: converter logic

--- CurrencyConverter/currency_converter.py
+++ CurrencyConverter/currency_converter.py
@@ -3,42 +3,48 @@ import os
 
 load_dotenv()
 
-def get_values_and_convert():
-    try:
-        value = float(input(os.environ.get('GET_VALUE') + ' '))
-        old_currency = str(input(os.environ.get('GET_OLD') + ' ')).upper()
-        old_symbol = '$' if old_currency=='USD' else 'R$' if old_currency=='BRL' else '€'
-        currency = str(input(os.environ.get('GET_NEW') + ' ')).upper()
-        if(old_currency==currency):
-            raise ValueError
-        symbol = '$' if currency=='USD' else 'R$' if currency=='BRL' else '€'
-        conversion_tax = float(input(os.environ.get('GET_TAX') + ' '))
-        return {'origin': value, 'value': value*conversion_tax, 'old_currency': old_currency, 'old_symbol': old_symbol, 'currency': currency, 'symbol': symbol, 'tax': conversion_tax}
-    except ValueError:
-        print('You cannot use the same currency two times.')
-    
-def save_operation_history(value_to_append):
+def print_and_save_operation_history(operation):
+    print(operation)
     history = open('./values.txt', 'a')
-    history.write(value_to_append)
+    history.write(f'{operation}\n')
     history.close()
 
+def clear_history():
+    history = open('./values.txt', 'w')
+    history.write('')
+
 def show_options():
-    option = int(input(os.environ.get('OPTION') + ' '))
+    option = int(input(f"{os.environ.get('OPTION')} "))
     return option
 
-while True:
-    try:
-        conversion_result = get_values_and_convert()
-        format_value = f'VALUE {conversion_result['old_symbol']}{conversion_result['origin']}\nFROM {conversion_result['old_currency']}\nTO {conversion_result['currency']}\nUSING TAX {conversion_result['old_symbol']}{conversion_result['tax']}\nEQUALS {conversion_result['symbol']}{conversion_result['value']}.'
-        save_operation_history(format_value)
-        print(format_value)
+def get_values_and_convert():
+    currencies = ['EUR', 'USD', 'JPY']
+    while True:
+        try:
+            print(f'{'-'*5} BRL CONVERTER [TYPE CTRL + C TO EXIT ANY TIME!] {'-'*5}')
+            value = float(input(f'{os.environ.get('GET_VALUE')} '))
 
-        options = show_options()
-        if(options==2):
-            print('Bye')
+            currency = str(input(f'{os.environ.get('GET_NEW')} ')).upper()
+
+            if currency not in currencies:
+                print('Invalid currency value. Try again.')
+            else:
+                symbol = '$' if currency=='USD' else '¥' if currency=='JPY' else '€'
+
+                conversion_tax = 0.15947581 if currency=='EUR' else 0.16604882 if currency=='USD' else 25.870705
+
+                print_and_save_operation_history(f'R${value:.2f} -> {symbol}{value*conversion_tax:.2f}')
+
+                option = show_options()
+                if option == 2:
+                    clear_history()
+                    print('History successfully cleared. Running the converter once again.')
+                elif option == 3: break
+
+        except ValueError:
+            print(f'{os.environ.get('VALUE_ERROR')} ')
+        except:
+            print('Something went wrong. Stopping application.')
             break
-    except ValueError:
-        print(os.environ.get('VALUE_ERROR'))
-    except:
-        print('Something went wrong. Stopping application.')
-        break
+    
+get_values_and_convert()
